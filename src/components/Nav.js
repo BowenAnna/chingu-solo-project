67fd51a0a581d94fcda4c415153dd2f5e0d68846
BowenAnna <86504623+BowenAnna@users.noreply.github.com>
2024-08-25 10:07:29 -0400

    Created a fecth request

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Link, Route, Routes} from "react-router-dom";
+import Questions from './Questions';
 
-export default function Nav() {
+
+const Nav=()=>{
+const[data, setData]=useState([]);
+const[loading, setLoading]=useState(true);
+const[error, setError]=useState(null);
+
+useEffect(()=>{
+  fetch("https://johnmeade-webdev.github.io/chingu_quiz_api/trial.json")
+  .then(response =>{
+    if(!response.ok){
+      throw new Error("Network response wasn't ok")
+    }
+    return response.json();
+  })
+  .then(data => {
+    setData(data);
+    setLoading(false);
+  })
+  .catch(error => {
+    setError(error);
+    setLoading(false);
+  });
+}, []);
+
+if (loading) {
+return <div>Loading...</div>;
+}
+
+if (error) {
+return <div>Error: {error.message}</div>;
+}
+
   return (
     <div>
       <h1>Chingu Trivia</h1>
@@ -13,7 +45,11 @@ export default function Nav() {
           <Link to="/questions">Questions</Link>
         </li>
       </ul>
-
+    <Routes>
+      <Route path=""/>
+      <Route path="/questions" element={<Questions/>}/>
+    </Routes>
     </div>
   )
 }
+export default Nav;
