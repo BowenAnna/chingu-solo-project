89f6601781f78d89086684b97f196cba1710b015
BowenAnna <86504623+BowenAnna@users.noreply.github.com>
2024-08-25 22:57:06 -0400

    updated styling of the home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
-    <div>Welcome to the Chingu Trivia</div>
-  )
+    <div class="main-home">
+      <h1>Welcome to the Chingu Trivia</h1>
+      <p>
+        Navigate to{" "}
+        <button>
+          <Link to="/questions" style={{ color: "black" }}>
+            "Questions"
+          </Link>
+        </button>{" "}
+        to start the game
+      </p>
+    </div>
+  );
 }
