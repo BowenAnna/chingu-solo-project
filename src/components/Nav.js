5c1050a5ab081c5431157095c02995c4b29df873
BowenAnna <86504623+BowenAnna@users.noreply.github.com>
2024-08-25 20:46:28 -0400

    Displaying only one question out of ten at a time

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,6 @@
-import React, {useState, useEffect} from 'react';
-import {Link, Route, Routes} from "react-router-dom";
-import Questions from './Questions';
-import Home from './Home';
+import React from 'react';
+import {Link} from "react-router-dom";
+
 
 
 const Nav=()=>{
--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,24 +1,30 @@
 import React from 'react'
+import { useState } from 'react';
 
 export default function Questions({data}) {
     const [answer, setAnswer]=useState({});
-    const [result, setRResult]=useState(null);
+    const [result, setResult]=useState(null);
+    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+    const tenQuestion = data.slice(0,10);
+    const currentQuestion = tenQuestion[currentQuestionIndex];
 
   return (
     <div>
-    <p>Questions{data.id}</p>
-    {data.map((item) => (
-      <div key={item.id}>
-        <p>{item.question}</p>
-        <ul>
-          {Object.entries(item.choices).map(([key, choice]) => (
-            <li key={key}>
-                <button> {key}: {choice}</button>
-            </li>
-          ))}
-        </ul>
-      </div>
-    ))}
+    <p>Questions {currentQuestionIndex + 1}/{tenQuestion.length}</p>
+    {currentQuestion?(
+ <div>
+ <p>{currentQuestion.question}</p>
+ <ul>
+   {Object.entries(currentQuestion.choices).map(([key, choice]) => (
+     <li key={`choice-${currentQuestion.id}-${key}`}>
+         <button> {key}: {choice}</button>
+     </li>
+   ))}
+ </ul>
+</div>
+    ):(<div>Loading...</div>)
+     
+   }
   </div>
   )
 }
