d17482b3d848483c1b226be465bf5b0125746611
BowenAnna <86504623+BowenAnna@users.noreply.github.com>
2024-08-25 21:01:23 -0400

    Score responsefor the correct or wrong answers

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,30 +1,47 @@
-import React from 'react'
-import { useState } from 'react';
+import React from "react";
+import { useState } from "react";
 
-export default function Questions({data}) {
-    const [answer, setAnswer]=useState({});
-    const [result, setResult]=useState(null);
-    const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-    const tenQuestion = data.slice(0,10);
+export default function Questions({ data }) {
+  const [answer, setAnswer] = useState({});
+  const [result, setResult] = useState(null);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const tenQuestion = data.slice(0, 10);
+
+  const [score, setScore]=useState(null);
+
+  const handleChoiceClick = (choiceKey)=>{
     const currentQuestion = tenQuestion[currentQuestionIndex];
-
+    setAnswer(previousAnswers=>({...previousAnswers, [currentQuestion.id]:choiceKey}));
+    if(choiceKey === currentQuestion.answer){
+        setScore("Well done!")
+    }
+    else{
+        setScore("Try again next time.")
+    }
+  }
+  const currentQuestion = tenQuestion[currentQuestionIndex];
   return (
     <div>
-    <p>Questions {currentQuestionIndex + 1}/{tenQuestion.length}</p>
-    {currentQuestion?(
- <div>
- <p>{currentQuestion.question}</p>
- <ul>
-   {Object.entries(currentQuestion.choices).map(([key, choice]) => (
-     <li key={`choice-${currentQuestion.id}-${key}`}>
-         <button> {key}: {choice}</button>
-     </li>
-   ))}
- </ul>
-</div>
-    ):(<div>Loading...</div>)
-     
-   }
-  </div>
-  )
+      <p>
+        Questions {currentQuestionIndex + 1}/{tenQuestion.length}
+      </p>
+      {currentQuestion ? (
+        <div>
+          <p>{currentQuestion.question}</p>
+          <ul>
+            {Object.entries(currentQuestion.choices).map(([key, choice]) => (
+              <li key={`choice-${currentQuestion.id}-${key}`}>
+                <button onClick={()=>handleChoiceClick(key)}>
+                  {key}: {choice}
+                </button>
+              </li>
+            ))}
+          </ul>
+          {score && <p>{score}</p>}
+        </div>
+      ) : (
+        <div>Loading...</div>
+      )}
+    </div>
+  );
 }
